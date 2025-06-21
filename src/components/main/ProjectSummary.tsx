aa1559884a57e70cd28570a4e992ea4c215ea68c
rkang427 <rkang160@gmail.com>
2025-06-21 18:47:43 -0400

    feat(main): adjusted minor spacing, added placeholder projects in Project Component

--- src/components/main/ProjectSummary.tsx
+++ src/components/main/ProjectSummary.tsx
@@ -18,33 +18,47 @@ export default function ProjectSummary() {
         {/*  height={300}*/}
         {/*/>*/}
         <ContributionGraph />
-
+          <h1 className="font-semibold">Recent Projects</h1>
         <div className="md:flex justify-center gap-10 mt-3">
           <Box
             sx={{
-              width: 150,
-              height: 160,
+              width: 180,
+              height: 140,
               bgcolor: 'var(--superLightGray)',
               borderRadius: 4,
             }}
           >
-            <div className="flex justify-end items-start m-2 space-x-2">
+            <div className="flex justify-end items-start m-1 space-x-2">
+                <a href="https://github.com/rkang427/TSA" target="_blank" rel="noopener noreferrer">
               <CodeIcon className="align-right rounded-full  hover:border-pine hover:text-lime   border-darkGray ml-auto  " />
+                </a>
+                <a href="https://thescholarshipacademy2025report.vercel.app/" target="_blank" rel="noopener noreferrer">
               <ArrowOutwardIcon className="align-right rounded-full bg-lime   border-darkGray ml-auto hover:bg-lightGray" />
+                </a>
             </div>
+              <div className="mx-2 mb-3">
+              <h1 className="mb-1 font-semibold">The Scholarship Academy</h1>
+                  <p className="m-2 font-light text-xs">A non-profit dashboard for school, district, and statewide impact.</p></div>
           </Box>
           <Box
             sx={{
-              width: 150,
-              height: 160,
+              width: 180,
+              height: 140,
               bgcolor: 'var(--lightGray)',
               borderRadius: 4,
             }}
           >
-            <div className="flex justify-end items-start m-2 space-x-2">
-              <CodeIcon className="align-right rounded-full hover:border-pine hover:text-lime   border-darkGray ml-auto  " />
-              <ArrowOutwardIcon className="align-right rounded-full bg-lime   border-darkGray ml-auto hover:bg-lightGray" />
+            <div className="flex justify-end items-start m-1 space-x-2">
+                <a href="https://github.com/rkang427/crAIvings" target="_blank" rel="noopener noreferrer">
+                    <CodeIcon className="align-right rounded-full  hover:border-pine hover:text-lime   border-darkGray ml-auto  " />
+                </a>
+                <a href="https://craivings.vercel.app/" target="_blank" rel="noopener noreferrer">
+                    <ArrowOutwardIcon className="align-right rounded-full bg-lime   border-darkGray ml-auto hover:bg-lightGray" />
+                </a>
             </div>
+              <div className="m-1">
+              <h1 className="mx-1 mb-1 font-semibold">CrAIVings</h1>
+              <p className="mx-2 font-light text-xs">Serverless DB recommending restaurants based on token.</p></div>
           </Box>
         </div>
       </div>
